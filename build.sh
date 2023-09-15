846c283776dfe1fae0b5f8de6300db30e7e09b03
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2023-09-15 09:34:04 -0700

    RpcCommandVariantsAsJson sample

---
+++ build.sh
@@ -0,0 +1,17 @@
+sudo apt-get install curl zip unzip tar
+cd deps/
+git clone -b feature/v2 https://github.com/Azure/azure-sdk-for-c
+git clone https://github.com/microsoft/vcpkg
+cd vcpkg
+./bootstrap-vcpkg.sh
+export VCPKG_ROOT=$(pwd)
+export VCPKG_DEFAULT_TRIPLET=x64-linux
+cp ../azure-sdk-for-c/vcpkg.json .
+./vcpkg install
+cd ../..
+export AZURE_SDK_DISABLE_AUTO_VCPKG=1
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DAZ_MQTT_TRANSPORT_IMPL=MOSQUITTO -DAZ_PLATFORM_IMPL=POSIX -DTRANSPORT_MOSQUITTO=ON -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake ..
+cmake --build .
+
